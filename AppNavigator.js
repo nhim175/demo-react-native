45a499666261aa2edf25e459798046b754dc2ca6
Thinh Pham <nhim175@gmail.com>
2019-05-31 15:29:03 +0700

    Add icon

--- AppNavigator.js
+++ AppNavigator.js
@@ -3,6 +3,7 @@ import {
   createStackNavigator,
   createBottomTabNavigator
 } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Category from './screens/Category';
 import Categories from './screens/Categories';
@@ -10,26 +11,55 @@ import Cart from './screens/Cart';
 import Orders from './screens/Orders';
 import Settings from './screens/Settings';
 
+const color = {
+  ACTIVE: '#147efb',
+  INACTIVE: '#ccc'
+}
+
 const CategoryStack = createStackNavigator({
   Categories,
   Category
 });
 CategoryStack.navigationOptions = {
-  tabBarLabel: 'Home'
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => {
+    return <Icon name="ios-planet"
+      size={36}
+      color={focused ? color.ACTIVE : color.INACTIVE}
+    />
+  }
 };
 const CartStack = createStackNavigator({
   Cart
 });
 CartStack.navigationOptions = {
-  tabBarLabel: 'Cart'
+  tabBarLabel: 'Cart',
+  tabBarIcon: ({ focused }) => {
+    return <Icon name="ios-cart"
+      size={36}
+      color={focused ? color.ACTIVE : color.INACTIVE}
+    />
+  }
 };
 const OrderStack = createStackNavigator({ Orders });
 OrderStack.navigationOptions = {
-  tabBarLabel: 'Orders'
+  tabBarLabel: 'Orders',
+  tabBarIcon: ({ focused }) => {
+    return <Icon name="ios-wallet"
+      size={36}
+      color={focused ? color.ACTIVE : color.INACTIVE}
+    />
+  }
 };
 const SettingStack = createStackNavigator({ Settings });
 SettingStack.navigationOptions = {
-  tabBarLabel: 'Settings'
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => {
+    return <Icon name="ios-cog"
+      size={36}
+      color={focused ? color.ACTIVE : color.INACTIVE}
+    />
+  }
 };
 
 const AppNavigator = createBottomTabNavigator({
